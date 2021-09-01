b3dbf47969063f78526c12c36a3daa73b91c26f9
muhammad <muhammadfallahi6@gmail.com>
2021-09-01 20:04:29 +0430

    post migration update

--- database/migrations/2021_09_01_151102_create_posts_table.php
+++ database/migrations/2021_09_01_151102_create_posts_table.php
@@ -15,6 +15,9 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete();
             $table->timestamps();
         });
     }
